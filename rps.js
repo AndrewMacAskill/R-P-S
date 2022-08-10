62fcf22bc283aed510c636bcb2f43984d9dc5059
Andrew MacAskill <andrewmacasill.16@gmail.com>
2022-08-09 21:23:36 -0400

    Randomizer Made

--- rps.js
+++ rps.js
@@ -6,8 +6,14 @@ const readline = require('readline').createInterface({
 // asks the user to enter their move and gets input
 readline.question('Enter your move (r/p/s)', move => {
     console.log(`Your move is ${move}`);
+    const computermove = getRandomInt(3)
+    const cmoves = ["r","p","s"]
+    const cmovesrps = cmoves [computermove]
+    console.log (`move is ${cmovesrps}`)
 });
-
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 
 
 
