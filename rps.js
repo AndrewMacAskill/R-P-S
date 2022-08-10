d1e8195067ec65e1217baa8752f2bd2130e64adf
Nick Simone <nick.simone100@gmail.com>
2022-08-09 21:03:03 -0400

    added input reader

--- rps.js
+++ rps.js
@@ -1,6 +1,12 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
-
-
+// asks the user to enter their move and gets input
+readline.question('Enter your move (r/p/s)', move => {
+    console.log(`Your move is ${move}`);
+});
 
 
 
